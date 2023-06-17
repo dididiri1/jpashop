52d31b9befdf15f60349e5d6f5e8165f02f2e47d
dididiri1 <dididiri1@naver.com>
2023-06-17 12:22:10 +0900

    주문 목록 API v1, v2 추가

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -54,7 +54,6 @@ public class MemberApiController {
 
     @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid CreateMemberRequest request){
-
         Member member = new Member();
         member.setName(request.getName());
 
